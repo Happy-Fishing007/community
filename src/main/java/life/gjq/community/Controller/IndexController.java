64a42f06779ac013c462ffc41e1ecfb0332e408c
“Happy-Fishing007” <gongjq2018@qq.com>
2022-02-17 17:22:30 +0800

    add

--- src/main/java/life/gjq/community/Controller/IndexController.java
+++ src/main/java/life/gjq/community/Controller/IndexController.java
@@ -1,20 +1,18 @@
 package life.gjq.community.Controller;
 
 
-import life.gjq.community.dto.QuestionDTO;
-import life.gjq.community.mapper.QuestionMapper;
+import life.gjq.community.dto.PaginationDTO;
 import life.gjq.community.mapper.UserMapper;
-import life.gjq.community.model.Question;
 import life.gjq.community.model.User;
 import life.gjq.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -31,6 +29,8 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
+                        @RequestParam(value = "page",defaultValue = "1") Integer page,
+                        @RequestParam(value = "size",defaultValue = "2") Integer size,
     Model model) {
         Cookie[] cookies = request.getCookies();
         if(cookies!=null){
@@ -45,8 +45,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
--- src/main/java/life/gjq/community/mapper/QuestionMapper.java
+++ src/main/java/life/gjq/community/mapper/QuestionMapper.java
@@ -3,15 +3,21 @@ package life.gjq.community.mapper;
 import life.gjq.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface QuestionMapper {
-    @Select("select * from question")
-    List<Question> list();
+    @Select("select count(1) from question")
+    Integer count();
+
+    @Select("select * from question limit #{offset},#{size}")
+    List<Question> list(@Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
 
     @Insert("insert into question(creator,title,description,get_create,get_modified,tag) values(#{creator},#{title},#{description},#{getCreate},#{getModified},#{tag})")
     void  create (Question question);
+
+
 }
