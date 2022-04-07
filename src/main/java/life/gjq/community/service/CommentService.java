429bada92bbdd42b62996f14e836f8e28600074b
“Happy-Fishing007” <gongjq2018@qq.com>
2022-04-07 12:28:56 +0800

    Fix multiplayer login problem

--- src/main/java/life/gjq/community/service/CommentService.java
+++ src/main/java/life/gjq/community/service/CommentService.java
@@ -30,7 +30,7 @@ public class CommentService {
     @Autowired
     private NotificationMapper notificationMapper;
 
-    public void insert(Comment comment,User commentator) {
+    public void insert(Comment comment, User commentator) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
         }
@@ -43,7 +43,7 @@ public class CommentService {
             //回复评论
             Comment dbComment = commentMapper.selectByPrimaryKey(comment.getParentId());
             Question question = questionMapper.selectByPrimaryKey(dbComment.getParentId());
-            if(question==null){
+            if (question == null) {
                 throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
             }
             //创建通知
@@ -51,24 +51,28 @@ public class CommentService {
 
             if (dbComment == null) {
                 throw new CustomizeException(CustomizeErrorCode.COMMENT_NOT_FOUND);
-            }else{
+            } else {
                 commentMapper.insert(comment);
             }
         } else {
             //回复问题
             Question question = questionMapper.selectByPrimaryKey(comment.getParentId());
-            if(question==null){
+            if (question == null) {
                 throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
             }
-                commentMapper.insert(comment);
-                question.setCommentCount(1);
-                questionExtMapper.incCommentCount(question);
+            commentMapper.insert(comment);
+            question.setCommentCount(1);
+            questionExtMapper.incCommentCount(question);
             //创建通知
-                createNotify(comment,question.getCreator(),commentator.getName(),question.getTitle( ), NotificationTypeEnum.REPLY_QUESTION, question.getId());
+            createNotify(comment, question.getCreator(), commentator.getName(), question.getTitle(), NotificationTypeEnum.REPLY_QUESTION, question.getId());
         }
     }
 
     private void createNotify(Comment comment, Long receiver, String notifierName, String outerTitle, NotificationTypeEnum notificationType, Long outerId) {
+      if(receiver==comment.getCommentator()){
+          return;
+      }
+
         Notification notification = new Notification();
         notification.setGmtCreate(System.currentTimeMillis());
         notification.setType(notificationType.getType());
@@ -81,8 +85,8 @@ public class CommentService {
         notificationMapper.insert(notification);
     }
 
-    public Map<Integer, List<CommentDTO>> getSecondAll(Long id,CommentTypeEnum type) {
-        if(type.getType()==1) {
+    public Map<Integer, List<CommentDTO>> getSecondAll(Long id, CommentTypeEnum type) {
+        if (type.getType() == 1) {
             Question question = questionMapper.selectByPrimaryKey(id);
             if (question.getCommentCount() == 0) {
                 return null;
@@ -94,17 +98,17 @@ public class CommentService {
                 .andParentIdEqualTo(id)
                 .andTypeEqualTo(type.getType());
         List<Comment> comments = commentMapper.selectByExample(example);
-        ArrayList<Long>  ids = new ArrayList<>();
+        ArrayList<Long> ids = new ArrayList<>();
 
         for (Comment comment : comments) {
             ids.add(comment.getId());
         }
         HashMap<Integer, List<CommentDTO>> integerListHashMap = new HashMap<>();
 
-        Integer i=0;
+        Integer i = 0;
         for (Long cid : ids) {
-            List<CommentDTO> commentDTOListById =getCommentDTOListById(cid, CommentTypeEnum.COMMENT);
-            integerListHashMap.put(i,commentDTOListById);
+            List<CommentDTO> commentDTOListById = getCommentDTOListById(cid, CommentTypeEnum.COMMENT);
+            integerListHashMap.put(i, commentDTOListById);
             i++;
         }
         return integerListHashMap;
@@ -112,27 +116,27 @@ public class CommentService {
 
     public List<CommentDTO> getCommentDTOListById(Long id, CommentTypeEnum type) {
         //查看点赞数是否为0，不用下面的comments判断，是因为有点bug,且不会解决
-        if(type.getType()==1){
+        if (type.getType() == 1) {
             Question question = questionMapper.selectByPrimaryKey(id);
-            if(question.getCommentCount()==0){
-                return  new ArrayList<>();
+            if (question.getCommentCount() == 0) {
+                return new ArrayList<>();
             }
-        }else{
+        } else {
             CommentExample example = new CommentExample();
             example.createCriteria()
-                            .andParentIdEqualTo(id)
-                                    .andTypeEqualTo(type.getType());
+                    .andParentIdEqualTo(id)
+                    .andTypeEqualTo(type.getType());
             List<Comment> comments = commentMapper.selectByExample(example);
-            if (comments.size()==0){
-                return  new ArrayList<>();
+            if (comments.size() == 0) {
+                return new ArrayList<>();
             }
         }
 
         //拿到所有parentId下面的回复
         CommentExample example = new CommentExample();
         example.createCriteria()
-                        .andParentIdEqualTo(id)
-                                .andTypeEqualTo(type.getType());
+                .andParentIdEqualTo(id)
+                .andTypeEqualTo(type.getType());
         List<Comment> comments = commentMapper.selectByExample(example);
 
         //拿到所有的评论者id的结果集(所有评论者)
@@ -143,15 +147,15 @@ public class CommentService {
         userIds.addAll(commentators);
         UserExample userExample = new UserExample();
         userExample.createCriteria()
-                        .andIdIn(userIds);
+                .andIdIn(userIds);
         List<User> users = userMapper.selectByExample(userExample);
-      //  System.out.println(users.get(0).getId());
+        //  System.out.println(users.get(0).getId());
         //将list集合存到Map中
         Map<Long, User> userMap = users.stream().collect(Collectors.toMap(user -> user.getId(), user -> user));
         //两个集合的键是相同的，通过comment的键值去userMap集合中取到对应的值
         List<CommentDTO> collectDTOS = comments.stream().map(comment -> {
             CommentDTO commentDTO = new CommentDTO();
-            BeanUtils.copyProperties(comment,commentDTO);
+            BeanUtils.copyProperties(comment, commentDTO);
             commentDTO.setUser(userMap.get(comment.getCommentator()));
             return commentDTO;
         }).collect(Collectors.toList());
@@ -159,15 +163,14 @@ public class CommentService {
     }
 
 
-
     public void incLikeCount(Comment comment) {
         commentExtMapper.incLikeCount(comment);
 
- }
+    }
 
     public Comment getComment(long id) {
-   Comment comment = commentMapper.selectByPrimaryKey(id);
-   return  comment;
+        Comment comment = commentMapper.selectByPrimaryKey(id);
+        return comment;
     }
 
 
